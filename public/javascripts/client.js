6041add58213bb0f3a0bec34fcd3585b6c322b86
Stefan Valentin <khalilravanna@gmail.com>
2012-12-13 21:19:11 -0500

    newest changes

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,4 +1,35 @@
 $(function(){
+	var firstImageAdded = false;
+	var startOver = false;
+	var content;
+	$("a[rel=tooltip]").tooltip();
+
+	$(".dropdown-item1").click(function(){
+		var newTitle = $(this).html();
+		$(".dropdown-target1").html(newTitle);
+	});
+	$(".dropdown-item2").click(function(){
+		var newTitle = $(this).html();
+		$(".dropdown-target2").html(newTitle);
+	});
+
+	$("#rate-btn").click(function(){
+		if(startOver === false){
+			var currentItems = $("#side-images").html();
+			$("#side-images").html('<div class="thumbnail"><img id="upload-pic" src="http://placehold.it/240x140" alt=""></div>');
+			content = $("#rating-location").html();
+			$("#rating-location").html('<p class="lead">Our rating: 85%</p><h3>Your outfit:</h3>' + 
+				currentItems);
+			$("#rate-btn").removeClass('btn-success').addClass('btn-danger').html('<i class="icon-remove-circle"></i> Start Over');
+			startOver = true;
+		}
+		else{
+			$("#rating-location").html(content);
+			$("#rate-btn").removeClass('btn-danger').addClass('btn-success').html('<i class="icon-hand-right"></i> Rate This Outfit');
+			startOver = false;
+		}
+	});
+
 	$("#upload-btn").click(function(){
 		$("#upload-pic").attr('src', 'images/dope-sweater.jpg');
 	});
@@ -6,17 +37,30 @@ $(function(){
 	$("#recommend-btn").click(function(){
 		$("#shirt").attr('src', 'images/blazer.jpg');
 		$("#pants").attr('src', 'images/blue-pants.jpg');
+		$("#rating").html('83%');
 	});
+
+	$("#details-btn").toggle(function(){
+		$("#details-text").html('<p>Blue shirt by some company</p><p>Some khakis by some other company</p>');
+	},function(){
+		$("#details-text").html('');
+	})
 
 	$("a.thumbnail").click(function(){
-		var image = $(this).clone();
-		image.attr('height', '140').attr('width','140');
-		image.addClass('side-thumb');
-		$("#side-images").append(image);
-	});
-
-	$("a.side-thumb").click(function(){
-		$(this).remove();
+		if($(this).hasClass('side-thumb')){
+			console.log("click delete");
+			$(this).remove();
+		}
+		else{
+			if(firstImageAdded === false){
+				firstImageAdded = true;
+				$("#side-images").html('');
+			};
+			var image = $(this).clone();
+			image.attr('height', '140').attr('width','140');
+			image.addClass('side-thumb');
+			$("#side-images").append(image);
+		}
 	});
 
 	$("#filter").click(function(){
